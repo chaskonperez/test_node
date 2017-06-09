ede163579b1798d374f65fd0cc860682e50a114f
Pedro Pérez <pedroperez1791@gmail.com>
2017-06-09 12:47:36 -0400

    test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # test_node
+
+- Ingresar a http://www.resti.cl:3000/test_node/indicadores
---
+++ server.js
@@ -0,0 +1,35 @@
+﻿var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var mysql = require('mysql');
+
+var app = express();
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "prueba"
+});
+
+app.get('/indicadores', function (req, res) {
+    request('http://si3.bcentral.cl/Indicadoressiete/secure/Indicadoresdiarios.aspx', function (error, response, html) {
+        var $ = cheerio.load(html);
+
+        var json = {
+            UF: $('#lblValor1_1').text(),
+            USD: $('#lblValor1_3').text(),
+            EUR: $('#lblValor1_5').text()
+        };
+
+        con.connect(function (err) {
+            con.query("INSERT INTO indicadores (uf, usd, eur) VALUES ('" + json.UF + "','" + json.USD + "','" + json.EUR + "')");
+        });
+
+        res.json(json);
+    });
+});
+
+app.listen(3000, function () {
+    console.log('listening on port 3000!');
+});
